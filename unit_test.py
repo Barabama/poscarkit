41ea050d419cb04030edcc381d1ead6d7e1bf236
Barabama <barabama@qq.com>
2025-07-22 11:54:05 +0800

    低：调整模版

--- unit_test.py
+++ unit_test.py
@@ -1,58 +1,48 @@
 
-
 import logging
+import os
 
-from PoscarTools.Supercell import supercell2file
-from PoscarTools.AtomConvert import convert2file
-from PoscarTools.AtomSlice import slice2file
-from PoscarTools.AtomShuffle import shuffle2files
-from PoscarTools.AtomAllocate import allocate2file
+from PoscarTools.AtomSupercell import supercell2file
+from PoscarTools.AtomAllocate import allocate2files
 from PoscarTools.AtomCountCN import countCN2files
+from PoscarTools.AtomSlice import slice2file
 
 
-logging.basicConfig(level=logging.DEBUG, format="%(asctime)s [%(levelname)s] %(message)s")
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
+
+outdir = "examples/out"
+os.makedirs(outdir, exist_ok=True)
 
 
 def test_supercell():
     filepath = "examples/POSCAR-fcc.vasp"
-    factors = (2, 2, 2)
-    supercell2file(filepath, factors)
-    # make_supercell(filepath, factors)
-
-
-def test_convert():
-    filepath = "examples/CoNiV-222/POSCAR-r1-CoNiV.vasp"
-    direction = (1, 1, 0)
-    convert2file(filepath, direction)
-
-
-def test_slice():
-    filepath = "examples/POSCAR-fcc.vasp"
-    direction = (1, 1, 0)
-    slice2file(filepath, direction)
-
-
-def test_shuffle():
-    filepath = "examples/POSCAR-fcc-222.vasp"
-    structure = {"cell": [3.5, 3.5, 3.5],
-                 "1a": {"atoms": ["Au", [[0, 0, 0]]]},
-                 "3c": {"atoms": ["Cu", [[0, 0.5, 0.5], [0.5, 0, 0.5], [0.5, 0.5, 0]]]}}
-    seeds = [7, 42]
-    shuffle2files(filepath, structure, seeds)
+    factors = (3, 3, 3)
+    supercell2file(filepath=filepath, outdir=outdir, factors=factors)
 
 
 def test_allocate():
-    filepath = "examples/POSCAR-fcc-222.vasp"
-    structure = {"cell": [3.5, 3.5, 3.5],
-                 "1a": {"atoms": ["Au", [[0, 0, 0]]], "sofs": {"V": 1}},
-                 "3c": {"atoms": ["Cu", [[0, 0.5, 0.5], [0.5, 0, 0.5], [0.5, 0.5, 0]]],
-                        "sofs": {"Co": 0.4444, "Cr": 0.4444, "V": 0.1112}}}
-    allocate2file(filepath, structure, (2, 2, 2))
+    filepath = "examples/out/POSCAR-supercell-3x3x3.vasp"
+    factors = (3, 3, 3)
+    info = {"1a": {"atoms": ["Au", [[0, 0, 0]]], "sofs": {"V": 1}},
+            "3c": {"atoms": ["Cu", [[0, 0.5, 0.5], [0.5, 0, 0.5], [0.5, 0.5, 0]]],
+                   "sofs": {"Co": 0.4444, "Ni": 0.4444, "V": 0.1112}}}
+    # info = None
+    allocate2files(filepath=filepath, outdir=outdir, factors=factors, struct_info=info)
+
+
+def test_countCN():
+    filepath = "examples/out/POSCAR-allocate1-CoNiV.vasp"
+    countCN2files(filepath=filepath, outdir=outdir)
+
+
+def test_slice():
+    filepath = "examples/out/POSCAR-allocate1-CoNiV.vasp"
+    direction = (1, 1, 1)
+    slice2file(filepath=filepath, outdir=outdir, miller_index=direction)
 
 
 if __name__ == "__main__":
-    # test_supercell()
-    # test_slice()
-    # test_shuffle()
-    # test_allocate()
-    test_convert()
+    test_supercell()
+    test_allocate()
+    test_countCN()
+    test_slice()
